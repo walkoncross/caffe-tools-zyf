1f3cd1d48b6b105cc77ceef2e6cd821214f68517
zyf <zhaoyafei0210@gmail.com>
2017-12-11 01:15:41 +0800

    add parse_log_for_sphereface/*

--- convert_binaryproto_to_npy.py
+++ convert_binaryproto_to_npy.py
@@ -34,6 +34,7 @@ def binaryproto_to_npy(bp_file, npy_file=None):
 
     array = np.array(caffe.io.blobproto_to_array(blob))
     print 'loaded mean array shape: ', array.shape
+    print 'per-channel mean: ', mean_array.mean(2).mean(2)
 
     mean_npy = array[0]
     if npy_file is not None:
@@ -43,8 +44,13 @@ def binaryproto_to_npy(bp_file, npy_file=None):
 
 
 if __name__ == '__main__':
-    args = parse_argv(sys.argv)
-    bp_file = args.input_file
-    npy_file = args.output_file
+    if len(sys.argv) > 1:
+        args = parse_argv(sys.argv)
+        bp_file = args.input_file
+        npy_file = args.output_file
+    else:
+#        bp_file = r'C:\zyf\dnn_models\resnet-msra\ResNet_mean.binaryproto'
+        bp_file = r'C:\zyf\github\caffe\data\ilsvrc12\imagenet_mean.binaryproto'
+        npy_file = None
 
-    binaryproto_to_npy(bp_file, npy_file)
+    mean_array = binaryproto_to_npy(bp_file, npy_file)
---
+++ parse_log_for_sphereface/parse_and_plot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+LOGFILE=$1
+
+/disk2/zhaoyafei/parse_train_log/plot_training_log_for_sphereface.py 10 train_loss_vs_iters.png $LOGFILE
+/disk2/zhaoyafei/parse_train_log/plot_training_log_for_sphereface.py 8 train_lr_vs_iters.png $LOGFILE
+/disk2/zhaoyafei/parse_train_log/plot_training_log_for_sphereface.py 6 train_lambda_vs_iters.png $LOGFILE
+/disk2/zhaoyafei/parse_train_log/plot_training_log_for_sphereface.py 2 test_loss_vs_iters.png $LOGFILE
+/disk2/zhaoyafei/parse_train_log/plot_training_log_for_sphereface.py 0 test_acc_vs_iters.png $LOGFILE
+/disk2/zhaoyafei/parse_train_log/plot_training_log_for_sphereface.py 4 test_acc_top5_vs_iters.png $LOGFILE
